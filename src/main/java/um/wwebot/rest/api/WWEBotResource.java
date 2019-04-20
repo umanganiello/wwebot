ccc7f165fca69f766cb31e1b63a67fe7b2a0dc9e
Umberto Manganiello <manganielloumberto@gmail.com>
2019-04-20 13:33:53 +0200

    Refactoring

--- src/main/java/um/wwebot/rest/api/WWEBotResource.java
+++ src/main/java/um/wwebot/rest/api/WWEBotResource.java
@@ -1,11 +1,15 @@
 package um.wwebot.rest.api;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Component;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,12 +19,26 @@ import lombok.extern.slf4j.Slf4j;
 public class WWEBotResource {
 
 	public WWEBotResource(){
-     log.info("WWEBot resource started");     
-  }
+		log.info("WWEBot resource started");     
+	}
 
-  @GET
-  @Path("/{name}")
-  public Response getMessage(@PathParam("name") String name) {
-      return Response.ok().entity("Ciao "+name).build();
-  }
+//  @GET
+//  @Path("/{name}")
+//  public Response getMessage(@PathParam("name") String name) {
+//      return Response.ok().entity("Hello "+name).build();
+//  }
+	
+	@POST
+	@Path("/update/434ef3c8-fc00-4b70-945c-c2c756d9101a")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response getUpdate(String update) {
+		log.info("Received update:\n {}", update) ;
+		
+		String jsonPath = "$.address.street";
+		DocumentContext jsonContext = JsonPath.parse(update);
+		String street = jsonContext.read(jsonPath);
+		log.info("Street {}", street);
+		
+		return Response.ok().build();
+	}
 }
